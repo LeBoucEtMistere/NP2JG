f799c630d4081d30c35d2f4370c8ca6c8c611af0
Depasse Arthur <arthur.depasse96@gmail.com>
2019-05-13 20:46:39 +0200

    Modif de la génération asn1

--- fr.supelec.plong9.fsm.edit/src-gen/fr/supelec/plong9/fsm/provider/MessageItemProvider.java
+++ fr.supelec.plong9.fsm.edit/src-gen/fr/supelec/plong9/fsm/provider/MessageItemProvider.java
@@ -2,6 +2,7 @@
  */
 package fr.supelec.plong9.fsm.provider;
 
+import fr.supelec.plong9.fsm.FsmFactory;
 import fr.supelec.plong9.fsm.FsmPackage;
 import fr.supelec.plong9.fsm.Message;
 
@@ -12,6 +13,7 @@ import org.eclipse.emf.common.notify.Notification;
 
 import org.eclipse.emf.common.util.ResourceLocator;
 
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -104,6 +106,36 @@ public class MessageItemProvider extends ItemProviderAdapter implements IEditing
 	}
 
 	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(FsmPackage.Literals.MESSAGE__ENUMS_DEF);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
+	}
+
+	/**
 	 * This returns Message.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
@@ -154,6 +186,9 @@ public class MessageItemProvider extends ItemProviderAdapter implements IEditing
 		case FsmPackage.MESSAGE__NAME:
 			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 			return;
+		case FsmPackage.MESSAGE__ENUMS_DEF:
+			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+			return;
 		}
 		super.notifyChanged(notification);
 	}
@@ -168,6 +203,9 @@ public class MessageItemProvider extends ItemProviderAdapter implements IEditing
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add(createChildParameter(FsmPackage.Literals.MESSAGE__ENUMS_DEF,
+				FsmFactory.eINSTANCE.create(FsmPackage.Literals.STRING_TO_STRING_MAP)));
 	}
 
 	/**
